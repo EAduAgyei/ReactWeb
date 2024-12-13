a54f88c942c83ded08c5846a8f390a2ba643fcc0
Eunice <eunice.aduagyei01@gmail.com>
2024-12-13 12:47:00 -0500

    added interface PageProps

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++ app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
-const Page = ({params}: {params:{id:string}}) => {
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+const Page: React.FC<PageProps> = ({ params }) => {
+  const { id } = params;
+  return <h1 className="text-3xl">User Profile: {id}</h1>;
+  
+};
+
+export default Page;
+
+
+/*
+interface PageProps{
+params:{
+        id:string;
+    };
+}
+const Page: React.FC = ({params}) => {
     const {id} = params; 
-    return (
-    <h1 className="text-3xl">User Profile: {id} </h1>
+ return (
+    <h1 className="text-3xl">User Profile: {id}</h1>
     )
 }
-export default Page
+    export default Page;*/
+//
