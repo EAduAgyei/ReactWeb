8b749c8921148c24ef26b7e236a17545923730b0
Eunice <eunice.aduagyei01@gmail.com>
2024-12-16 23:05:18 -0500

    created lib folder

--- app/auth.ts
+++ app/auth.ts
@@ -1,6 +1,33 @@
 import NextAuth from "next-auth"
-import Resend from "next-auth/providers/resend"
+//import Resend from "next-auth/providers/resend"
+import Credentials from "next-auth/providers/credentials"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Resend],
+  providers: [Credentials({
+    // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+    // e.g. domain, username, password, 2FA token, etc.
+    credentials: {
+      email: {},
+      password: {},
+    },
+    authorize: async (credentials) => {
+      let user = null
+
+      // logic to salt and hash password
+      const pwHash = (credentials.password)
+
+      // logic to verify if the user exists
+      user = await (credentials.email, pwHash)
+
+      if (!user) {
+        // No user found, so this is their first attempt to login
+        // Optionally, this is also the place you could do a user registration
+        throw new Error("Invalid credentials.")
+      }
+
+      // return user object with their profile data
+      return user
+    },
+  }),
+],
 })
